861c6efaf579b73d6e8bf2f51d4e1e51266714be
Gabriel-Marchione <gabrielmarchione2@gmail.com>
2025-07-13 19:37:04 +0200

    firstCommit

---
+++ restartOnWifiResponse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#ping -c 1 -W 2 8.8.8.8 > debug.txt
+ping -c 1 -W 2 192.192.192.192 > debug.txt
+
+#controllo se ho un match della stringa nel file debug.txt
+if ! grep -q "1 received" debug.txt; then
+    #non a buon fine ho 1
+    #connesso ad internet#
+    echo "ciao"
+    pihole restardns
+fi
