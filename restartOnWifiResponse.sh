edf14c8fbe0e275ebe5ebc1be786cf4ff605d822
Gabriel-Marchione <gabrielmarchione2@gmail.com>
2025-07-23 00:31:29 +0200

    modificato script, aggiunto controllo con flag per verificare se la connessione è persa o meno

--- restartOnWifiResponse.sh
+++ restartOnWifiResponse.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-ping -c 1 -W 2 8.8.8.8 > debug.txt
-#ping -c 1 -W 2 192.192.192.192 > debug.txt
 
-#controllo se NON ho un match della stringa nel file debug.txt
-if ! grep -q "1 received" debug.txt; then
-    #non a buon fine ho 1
-    #connesso ad internet#
-    echo "ciao"
-    pihole restardns
-fi
-#se ho un match tutto funziona e non faccio nulla
+googleIpAddress=8.8.8.8
+fakeIpAddress=192.192.192.192
 
-LOGFILE="/home/gabriel/script/ensureWifiIsOnRasp/connessione.log"
+scriptDir="$(dirname "$0")" #con $(dirname "$0") mi prendo il nome della directory
+stateFile="/tmp/no_connection_flag"
+logFile="$scriptDir/log.txt"
 
-if ping -c 1 -W 2 8.8.8.8 > /dev/null 2>&1; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Connessione OK" >> "$LOGFILE"
-    # qui puoi aggiungere: sudo systemctl restart pihole-FTL.service
+if ! ping -c 1 -W 2 $googleIpAddress > /dev/null 2>&1; then
+    if [ ! f "$stateFile"]; then
+    echo echo "$(date) - Connessione PERSA" >> "$logFile" 
+    touch "$stateFile"
+    fi 
 else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Nessuna connessione" >> "$LOGFILE"
+    if [ -f "$stateFile"]; then
+        echo echo "$(date) - Connessione RISTABILITA" >> "$logFile" 
+        echo "pihole restartdns"
+    fi
 fi
+
+
