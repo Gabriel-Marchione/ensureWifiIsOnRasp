88d50187c07dfacd55e71fd1455ff7e17f04c70d
Gabriel-Marchione <gabrielmarchione2@gmail.com>
2025-07-23 00:52:42 +0200

    modificate parentesi

--- restartOnWifiResponse.sh
+++ restartOnWifiResponse.sh
@@ -8,13 +8,13 @@ stateFile="/tmp/no_connection_flag"
 logFile="$scriptDir/log.txt"
 
 if ! ping -c 1 -W 2 $googleIpAddress > /dev/null 2>&1; then
-    if [ ! f "$stateFile"]; then
+    if [ ! -f "$stateFile" ]; then
     echo echo "$(date) - Connessione PERSA" >> "$logFile" 
     touch "$stateFile"
     fi 
 else
-    if [ -f "$stateFile"]; then
-        echo echo "$(date) - Connessione RISTABILITA" >> "$logFile" 
+    if [ -f "$stateFile" ]; then
+        echo "$(date) - Connessione RISTABILITA" >> "$logFile" 
         echo "pihole restartdns"
     fi
 fi
