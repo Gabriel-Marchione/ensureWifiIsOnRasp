a29f2e44a0cfdb246379f4dcec60b145177f52a0
Gabriel-Marchione <gabrielmarchione2@gmail.com>
2025-07-23 01:20:08 +0200

    tolte varibili non usate

--- restartOnWifiResponse.sh
+++ restartOnWifiResponse.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 googleIpAddress=8.8.8.8
-fakeIpAddress=192.192.192.192
 
 scriptDir="$(dirname "$0")" #con $(dirname "$0") mi prendo il nome della directory
 stateFile="/tmp/no_connection_flag"
 logFile="$scriptDir/log.txt"
 
-if ! ping -c 1 -W 2 $googleIpAddress > /dev/null 2>&1; then
+if ! ping -c 1 -W 2 "$googleIpAddress" > /dev/null 2>&1; then
     if [ ! -f "$stateFile" ]; then
-    echo echo "$(date) - Connessione PERSA" >> "$logFile" 
+    echo "$(date) - Connessione PERSA" >> "$logFile" 
     touch "$stateFile"
     fi 
 else
     if [ -f "$stateFile" ]; then
         echo "$(date) - Connessione RISTABILITA" >> "$logFile" 
         echo "pihole restartdns"
+        rm "$stateFile"
     fi
 fi
 
